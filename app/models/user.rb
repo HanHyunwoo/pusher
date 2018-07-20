3417d312d316b353f14d6da3c07640ccb5cdc7b7
classtak <classtak@gmail.com>
2018-07-20 01:44:52 +0000

    model 설정

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  # 채팅이랑 1:N
+  has_many :chats
+  # 채팅방이랑 M:N
+  has_many :admissions
+  has_many :chat_rooms, through: :admissions
 end
